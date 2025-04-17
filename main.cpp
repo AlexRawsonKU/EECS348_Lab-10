c25410b882d15891e1853444b880d1f5d783402b
Alex Rawson <a02r956@ku.edu>
2025-04-16 21:30:45 -0500

    implement main function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <fstream>
 
 #include "calculator.hpp"
 
 int main(int argc, char *argv[])
 {
+    std::string filename;
+    std::cout << "Enter a filename: ";
+    std::cin >> filename;
+
+    std::ifstream file(filename);
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file." << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (!std::getline(file, line).eof())
+    {
+        if (validate_double(line))
+        {
+            std::cout << line << " is valid double" << std::endl;
+        }
+        else
+        {
+            std::cout << line << " is not valid double" << std::endl;
+        }
+    }
 }
